bb18c57090c916d919b3155a6f6944175041844c
zuizihad <ahmedxihad@gmail.com>
2020-08-28 22:28:09 +0600

    style

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
       
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
       <SearchField 
       placeholder="search"
       handleChange={this.handleChange}
