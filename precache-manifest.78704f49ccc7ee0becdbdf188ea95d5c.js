cc895a761f9c869b7b24626dcffd1c29f0d5f9a5
zuizihad <ahmedxihad@gmail.com>
2020-08-28 22:40:05 +0600

    Updates

---
+++ precache-manifest.78704f49ccc7ee0becdbdf188ea95d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb0f45d2ef4bf6d5fb0fea38ac379d4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eca8f13a9f0f69bdf316",
+    "url": "/monsters-rolodex/static/css/main.f7cb4a65.chunk.css"
+  },
+  {
+    "revision": "79ec6c39e0cbeec1fc24",
+    "url": "/monsters-rolodex/static/js/2.fafdc493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fafdc493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca8f13a9f0f69bdf316",
+    "url": "/monsters-rolodex/static/js/main.e643697f.chunk.js"
+  },
+  {
+    "revision": "599b5dcd66b7132382cd",
+    "url": "/monsters-rolodex/static/js/runtime-main.1d83bf06.js"
+  }
+]);
